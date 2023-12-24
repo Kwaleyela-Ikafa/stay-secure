1eadb7f0880952f6aac84cba2ecb348a0c760b23
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-12-24 22:50:46 +0200

    update navbar responsiveness

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import logo from '../../assets/images/logo.svg'
+import React from 'react';
+import logo from '../../assets/images/logo.svg';
 import {
   Flex,
   Box,
@@ -8,60 +8,73 @@ import {
   Button,
   Spacer,
   HStack,
-  Divider,
-  Image
-} from '@chakra-ui/react'
-import { 
+  Image,
+  useMediaQuery,
+} from '@chakra-ui/react';
+import {
   PhoneIcon,
   EmailIcon,
-  ArrowForwardIcon
-} from '@chakra-ui/icons'
+  ArrowForwardIcon,
+  HamburgerIcon,
+} from '@chakra-ui/icons';
+import NavMenu from './components/Menu';
 
 function NavBar() {
-  return (
-    <header>
-        <Box>
-          <Flex as="nav" pb={5} pt={5} alignItems="center">
-            <Box boxSize='74px'>
-              <Image src={logo} alt='Stay Secure Logo' />
-            </Box>
-              <Spacer />
-  
-              <HStack spacing="20px">
-                  <Button
-                    color="white"
-                    variant="link"
-                    display="flex"
-                    alignItems="center"
-                    p="10px"
-                    as="a"
-                    href="tel:+260977269184"
-                  >
-                    <Icon boxSize={3.5} as={PhoneIcon} />
-                    <Text pl={3}>+260 977 269 184</Text>
-                  </Button>
+  const [isSmallerThan768] = useMediaQuery('(max-width: 768px)');
 
-                  <Button
-                    color="white"
-                    variant="link"
-                    display="flex"
-                    alignItems="center"
-                    p="10px"
-                    as="a"
-                    href="mailto:staysecure@gmail.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Icon boxSize={3.5} as={EmailIcon} />
-                    <Text pl={3}>staysecure@gmail.com</Text>
-                  </Button>
-                  <Button borderRadius='50px' rightIcon={<ArrowForwardIcon />} colorScheme='blue' variant='outline'>Logout</Button>
-              </HStack>
-          </Flex>
-          <Divider pt={6} />
+  return (
+    <Box p="4" color="white">
+      <Flex as="nav" pb={5} pt={5} alignItems="center">
+        <Box boxSize={isSmallerThan768 ? '50px' : '80px'}>
+          <Image src={logo} alt="Stay Secure Logo" />
         </Box>
-    </header>
-  )
+        <Spacer />
+
+        {isSmallerThan768 ? (
+          <NavMenu />
+        ) : (
+          <HStack spacing="20px">
+            <Button
+              color="white"
+              variant="link"
+              display="flex"
+              alignItems="center"
+              p="10px"
+              as="a"
+              href="tel:+260977269184"
+            >
+              <Icon boxSize={3.5} as={PhoneIcon} />
+              <Text pl={3}>+260 977 269 184</Text>
+            </Button>
+
+            <Button
+              color="white"
+              variant="link"
+              display="flex"
+              alignItems="center"
+              p="10px"
+              as="a"
+              href="mailto:staysecure@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Icon boxSize={3.5} as={EmailIcon} />
+              <Text pl={3}>staysecure@gmail.com</Text>
+            </Button>
+            <Button
+              borderRadius="50px"
+              rightIcon={<ArrowForwardIcon />}
+              colorScheme="blue"
+              variant="outline"
+            >
+              Request a Quote
+            </Button>
+          </HStack>
+        )}
+      </Flex>
+      {!isSmallerThan768 && <hr />} {/* Render the divider for larger screens */}
+    </Box>
+  );
 }
 
 export default NavBar;
