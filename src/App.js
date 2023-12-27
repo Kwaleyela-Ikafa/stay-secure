7c001a2f086294dd1f177a0a87213ac3f1fc64cd
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-12-27 17:53:48 +0200

    import about component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './assets/css/header.css'
+import './assets/css/about.css'
 import NavBar from './components/NavBar/NavBar';
 import Header from './components/Header/Header';
+import About from './components/About/About';
 import {
   Box,
 } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className='header-center' >
-      <Box w='70%'>
-          <NavBar />
-          <Header />
-      </Box>
-    </div>
+    <>
+      <div className='header-center' >
+        <Box w='70%'>
+            <NavBar />
+            <Header />
+        </Box>
+      </div>
+
+      <div className='about-center' >
+        <About />
+      </div>
+    </>
   );
 }
 
